2a607798f8035d1e38c9bec6182968c5d6f86ebc
Seoyoung Lee <lsyamyseoyoung3040@gmail.com>
2026-05-12 01:45:20 +0900

    feat: 아이콘 사용을 위한 UIImage 익스텐션

---
+++ 38-COLLABORATION-iOS-DAANGN/38-COLLABORATION-iOS-DAANGN/Global/Extension/UIImage+.swift
@@ -0,0 +1,27 @@
+//
+//  UIImage+.swift
+//  38-COLLABORATION-iOS-DAANGN
+//
+//  Created by Seoyoung Lee on 5/11/26.
+//
+
+import UIKit
+
+extension UIImage {
+    static let chevrondown = UIImage(named: "chevron_down")
+    static let chevronLeft = UIImage(named: "chevron_left")
+    static let chevronRight = UIImage(named: "chevron_right")
+    static let chevronUp = UIImage(named: "chevron_up")
+    static let close = UIImage(named: "close")
+    static let crossHair = UIImage(named: "crosshair")
+    static let dot3VerticalFill = UIImage(named: "dot3_vertical_fill")
+    static let download = UIImage(named: "download")
+    static let filter = UIImage(named: "filter")
+    static let heartFill = UIImage(named: "heart_Fill")
+    static let heartLine = UIImage(named: "heart_line")
+    static let houseLine = UIImage(named: "house_line")
+    static let list = UIImage(named: "list")
+    static let map = UIImage(named: "map")
+    static let warningCircle = UIImage(named: "warningcircle")
+    static let xmarkCircleFill = UIImage(named: "xmark_circle_fill")
+}
