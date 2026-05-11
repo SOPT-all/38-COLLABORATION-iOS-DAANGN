6b02aa6d5536400cb08511b8766c6ad9eef3c0d3
kyooonnnggg <gyoeunjung23@sookmyung.ac.kr>
2026-05-11 18:38:27 +0900

    design: BottomSheet 관련 폴더링

---
+++ 38-COLLABORATION-iOS-DAANGN/38-COLLABORATION-iOS-DAANGN/Features/Detail/View/BottomSheet/BottomSheetViewController.swift
@@ -0,0 +1,7 @@
+//
+//  BottomSheetViewController.swift
+//  38-COLLABORATION-iOS-DAANGN
+//
+//  Created by 정교은 on 5/11/26.
+//
+
--- 38-COLLABORATION-iOS-DAANGN/38-COLLABORATION-iOS-DAANGN/Features/Detail/View/BottonBarView.swift
+++ 38-COLLABORATION-iOS-DAANGN/38-COLLABORATION-iOS-DAANGN/Features/Detail/View/BottonBarView.swift
@@ -1,5 +1,5 @@
 //
-//  BottonTabBar.swift
+//  BottomBarView.swift
 //  38-COLLABORATION-iOS-DAANGN
 //
 //  Created by 정교은 on 5/11/26.
