78f3f382cd18d333b871391dd1b7155d0cfaef1f
kyooonnnggg <gyoeunjung23@sookmyung.ac.kr>
2026-05-12 03:08:47 +0900

    design: 하단바 컴포넌트와 VC 연결

--- 38-COLLABORATION-iOS-DAANGN/38-COLLABORATION-iOS-DAANGN/Features/Detail/Controller/ProductDetailViewController.swift
+++ 38-COLLABORATION-iOS-DAANGN/38-COLLABORATION-iOS-DAANGN/Features/Detail/Controller/ProductDetailViewController.swift
@@ -6,6 +6,52 @@
 //
 
 import UIKit
+import SnapKit
 
-final class ProductDetailViewController : UIViewController {
+final class ProductDetailViewController: UIViewController {
+    
+    private let bottomBarView = BottomBarView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setStyle()
+        setUI()
+        setLayout()
+    }
+}
+
+private extension ProductDetailViewController {
+    
+    func setStyle() {
+        view.backgroundColor = .gray00
+    }
+    
+    func setUI() {
+        view.addSubview(bottomBarView)
+        bottomBarView.chatButton.addTarget(
+            self,
+            action: #selector(chatButtonDidTapped),
+            for: .touchUpInside
+        )
+    }
+    
+    func setLayout() {
+        bottomBarView.snp.makeConstraints {
+            $0.bottom.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(68)
+            $0.horizontalEdges.equalToSuperview()
+        }
+    }
+    
+    @objc
+    private func chatButtonDidTapped() {
+        presentBottomSheetVC()
+    }
+    
+    private func presentBottomSheetVC(){
+        let bottomSheetViewController = BottomSheetViewController()
+        bottomSheetViewController.modalPresentationStyle = .overFullScreen
+        self.present(bottomSheetViewController, animated: false)
+    }
 }
