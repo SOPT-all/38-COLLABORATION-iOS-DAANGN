6804b6bf7126f27b406c1f11012d23ccb9dda378
Seoyoung Lee <lsyamyseoyoung3040@gmail.com>
2026-05-12 18:46:22 +0900

    feat: 익스텐션 추가

---
+++ 38-COLLABORATION-iOS-DAANGN/38-COLLABORATION-iOS-DAANGN/Global/Extension/UITextField+.swift
@@ -0,0 +1,33 @@
+//
+//  UITextField.swift
+//  38-COLLABORATION-iOS-DAANGN
+//
+//  Created by Seoyoung Lee on 5/12/26.
+//
+
+import UIKit
+
+extension UITextField {
+    func addLeftPadding(_ width: CGFloat = 20) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: self.frame.height))
+        self.leftView = paddingView
+        self.leftViewMode = ViewMode.always
+    }
+    
+    func addRightPadding(_ width: CGFloat = 20) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: self.frame.height))
+        self.rightView = paddingView
+        self.rightViewMode = ViewMode.always
+    }
+    
+    func addRightButton(_ button: UIButton, padding: CGFloat = 4) {
+        let buttonSize: CGFloat = 24
+        let containerWidth = buttonSize + padding
+        let container = UIView(frame: CGRect(x: 0, y: 0, width: containerWidth, height: buttonSize))
+        button.frame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
+        container.addSubview(button)
+
+        self.rightView = container
+        self.rightViewMode = .always
+    }
+}
