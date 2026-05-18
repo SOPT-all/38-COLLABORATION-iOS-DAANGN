a6d07311575ae505090fcec14d0ef9845174b2cd
seoyeon <hemsyeons@gmail.com>
2026-05-18 18:47:02 +0900

    chore: BaseService 수정

---
+++ 38-COLLABORATION-iOS-DAANGN/38-COLLABORATION-iOS-DAANGN/Network/Base/BaseService.swift
@@ -0,0 +1,72 @@
+//
+//  BaseService.swift
+//  38-COLLABORATION-iOS-DAANGN
+//
+//  Created by 신서연 on 5/18/26.
+//
+
+import Foundation
+
+final class BaseService {
+
+    static let shared = BaseService()
+
+    private init() {}
+
+    func request<T: Decodable>(
+        endPoint: EndPoint
+    ) async throws -> T {
+        guard let baseURL = Bundle.main.infoDictionary?["BASE_URL"] as? String else {
+            throw NetworkError.invalidURL
+        }
+
+        let urlString = baseURL + endPoint.path
+
+        guard let encodedURL = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            throw NetworkError.invalidURL
+        }
+
+        guard let url = URL(string: encodedURL) else {
+            throw NetworkError.invalidURL
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = endPoint.method.rawValue
+
+        endPoint.header.forEach {
+            request.addValue($0.value, forHTTPHeaderField: $0.key)
+        }
+
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request)
+
+            guard let httpResponse = response as? HTTPURLResponse else {
+                throw NetworkError.invalidResponse
+            }
+
+            guard 200..<300 ~= httpResponse.statusCode else {
+                throw NetworkError.requestFailed(statusCode: httpResponse.statusCode)
+            }
+
+            do {
+                let decodedData = try JSONDecoder().decode(BaseResponseDTO<T>.self, from: data)
+
+                guard let data = decodedData.data else {
+                    throw NetworkError.emptyData
+                }
+
+                return data
+            } catch let error as NetworkError {
+                throw error
+            } catch {
+                print("디코딩 에러:", error)
+                throw NetworkError.decodingFailed
+            }
+
+        } catch let error as NetworkError {
+            throw error
+        } catch {
+            throw NetworkError.unknown
+        }
+    }
+}
